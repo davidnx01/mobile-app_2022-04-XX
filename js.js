e0f260b4d9c163c1b50f3e5f42b45a5f60892a18
davidnx01 <info@webdavid.sk>
2023-03-23 20:12:37 +0100

    commit

---
+++ js.js
@@ -0,0 +1,32 @@
+/* hamburger menu */
+
+function navOpen(){
+    let navOpen = document.getElementById("navOpen");
+    navOpen.classList.toggle("hamburger");
+
+    let whiteHamburger = document.getElementById("whiteHamburger");
+    whiteHamburger.classList.toggle("whitehamburger");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
